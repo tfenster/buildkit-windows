d4c0139374b01d6dcb33495ffe0acdd80992349d
Tobias Fenster <tfenster@4psbau.de>
2024-04-21 11:42:05 +0200

    add builder

--- SetupSampleDockerfile.ps1
+++ SetupSampleDockerfile.ps1
@@ -1,3 +1,5 @@
+Write-Host "Create Dockerfile etc."
+
 Set-Location "buildkit-windows-test\sample-dockerfile"
 Set-Content Dockerfile @"
 FROM mcr.microsoft.com/windows/nanoserver:ltsc2022
@@ -11,3 +13,7 @@ Set-Content hello.txt @"
 Hello from the buildkit Windows test scripts by Tobias Fenster!
 This message shows that your installation appears to be working correctly.
 "@
+
+Write-Host "Add builder"
+docker buildx create --name buildkit-exp --use --driver=remote npipe:////./pipe/buildkitd
+docker buildx inspect
